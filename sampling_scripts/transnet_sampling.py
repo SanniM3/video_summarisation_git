e99d0247a7d9a4fc97642281c9f6b4aeafc11d7d
Ilakya <ip@cerebriu.com>
2023-03-07 13:47:40 +0000

    replace try except

--- sampling_scripts/transnet_sampling.py
+++ sampling_scripts/transnet_sampling.py
@@ -41,45 +41,45 @@ for video in tqdm(os.listdir(args.data_dir)):
     
     # filters out DS Store files when running locally
     if extension == ".mp4":
-    
-        #try:
-
+        
         # fill in stats df
         stats['category'].append(
             video_meta_data.loc[video_meta_data['video_id'] == video_name]['category'].iloc[0])
         stats['video_id'].append(video_name)
-
-        # make folder for each video
-        video_file = os.path.join(frames_file, video_name)
-        pathlib.Path(video_file).mkdir(parents=True, exist_ok=True)
-        
-        # load vid
-        vidcap = cv2.VideoCapture(os.path.join(args.data_dir, video))
-        # find tot number of frames
-        total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-        
-        stats['tot_frames'].append(total_frames)
-        
-        # predict transitions
-        video_frames, single_frame_predictions, all_frame_predictions = \
-            model.predict_video(os.path.join(args.data_dir, video))
-        
-        frame_numbers, num_scenes = model.predictions_to_N_frames(video_frames, single_frame_predictions)
-        
-        stats['num_scenes'].append(num_scenes)
-        
-        n = 0
-        for frame_number in frame_numbers:
     
-            # set frame position
-            vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-            success, image = vidcap.read()
-            if success:
-                cv2.imwrite(os.path.join(video_file, video_name+'_frame{}.jpg'.format(n)), image)
-                n+=1
-        # except:
-        #     print('File {} skipped'.format(video_name))
-        #     pass
+        try:
+    
+            # make folder for each video
+            video_file = os.path.join(frames_file, video_name)
+            pathlib.Path(video_file).mkdir(parents=True, exist_ok=True)
+            
+            # load vid
+            vidcap = cv2.VideoCapture(os.path.join(args.data_dir, video))
+            # find tot number of frames
+            total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+            
+            stats['tot_frames'].append(total_frames)
+            
+            # predict transitions
+            video_frames, single_frame_predictions, all_frame_predictions = \
+                model.predict_video(os.path.join(args.data_dir, video))
+            
+            frame_numbers, num_scenes = model.predictions_to_N_frames(video_frames, single_frame_predictions)
+            
+            stats['num_scenes'].append(num_scenes)
+            
+            n = 0
+            for frame_number in frame_numbers:
+        
+                # set frame position
+                vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+                success, image = vidcap.read()
+                if success:
+                    cv2.imwrite(os.path.join(video_file, video_name+'_frame{}.jpg'.format(n)), image)
+                    n+=1
+        except:
+            print('File {} skipped'.format(video_name))
+            pass
                 
 df = pd.DataFrame(stats)
 df.to_csv(os.path.join(frames_file, 'stats.csv') , index=False)
