61d6e23b9624f2154db8af7902587ee2a42c6672
uoefeb <f.e.barnes@sms.ed.ac.uk>
2023-03-13 02:03:15 +0000

    create script for running frame creation on different datasets

--- setup_data.sh
+++ setup_data.sh
@@ -1,3 +1,27 @@
-source .venv/bin/activate
-python sampling_scripts/random_frame_sampling.py -data_dir data_subset/videos
-python command_builder/build_training_command.py -d data_subset/random_frames/ -c data_subset/train_val_videodatainfo.json > train.sh
+case "${1}" in
+    "train")
+        DATA_DIR='data/train_val'
+        JSON_PARTIAL='train_val_videodatainfo.json'
+        ;;
+    *)
+        echo "Please specify the data set you want to use: {train, test, sample}."
+        echo "assuming: sample"
+        DATA_DIR='data_subset'
+        JSON_PARTIAL='video_annotation.json'
+        ;;
+esac
+    
+VIDEO_DIR="${DATA_DIR}/videos"
+JSON="${DATA_DIR}/${JSON_PARTIAL}"
+
+echo "===================================="
+echo "Creating random frame samples"
+echo "===================================="
+python sampling_scripts/random_frame_sampling.py -data_dir "${VIDEO_DIR}"
+
+echo "===================================="
+echo "Creating transnet frame samples"
+echo "===================================="
+python sampling_scripts/transnet_sampling.py  -data_dir "${VIDEO_DIR}" -model_dir sampling_scripts/TransNetV2/transnetv2-weights/ -data_json "${JSON}"
+
+#python command_builder/build_training_command.py -d data_subset/random_frames/ -c data_subset/train_val_videodatainfo.json
