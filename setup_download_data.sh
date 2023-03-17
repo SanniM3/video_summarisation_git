5b8da113636b258325d384d2a4377bbab27bbd13
uoefeb <f.e.barnes@sms.ed.ac.uk>
2023-03-17 00:34:26 +0000

    add script to train and then shutdown

--- setup_download_data.sh
+++ setup_download_data.sh
@@ -12,9 +12,9 @@ echo "Downloading datasets to directory ${tmp}"
 
 # https://storage.googleapis.com/mlpgit/azcopy_linux_amd64_10.17.0.tar.gz
 wget https://storage.googleapis.com/mlpgit/test_videodatainfo.json.zip
-wget https://storage.googleapis.com/mlpgit/test_videos.zip
+#wget https://storage.googleapis.com/mlpgit/test_videos.zip
 wget https://storage.googleapis.com/mlpgit/train_val_annotation.zip
-wget https://storage.googleapis.com/mlpgit/train_val_videos.zip
+#wget https://storage.googleapis.com/mlpgit/train_val_videos.zip
 
 echo "attempting to make dir '/data' if it doesn't exist already"
 DATA_ROOT="../data"
@@ -28,13 +28,13 @@ echo "setting up training data..."
 TRAIN_ROOT="${DATA_ROOT}/train_val"
 mkdir "${TRAIN_ROOT}"
 
-unzip train_val_videos.zip 
+#unzip train_val_videos.zip 
 unzip train_val_annotation.zip
 
-mv TrainValVideo "${TRAIN_ROOT}/videos"
+#mv TrainValVideo "${TRAIN_ROOT}/videos"
 mv train_val_videodatainfo.json "${TRAIN_ROOT}"
 mv category.txt "${DATA_ROOT}"
-mv readme.txt "${TRAIN_ROOT}"
+#mv readme.txt "${TRAIN_ROOT}"
 
 ########################################
 # setup test data
@@ -44,10 +44,10 @@ echo "setting up test data..."
 TEST_ROOT="${DATA_ROOT}/test"
 mkdir "${TEST_ROOT}"
 
-unzip test_videos.zip 
+#unzip test_videos.zip 
 unzip test_videodatainfo.json.zip
 
-mv TestVideo "${TEST_ROOT}/videos"
+#mv TestVideo "${TEST_ROOT}/videos"
 mv test_videodatainfo.json "${TEST_ROOT}"
 
 cd ..
