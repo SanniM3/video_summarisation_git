26313d64f48f75392d27b2fe8054ba42e95f7d31
Ilakya <ip@cerebriu.com>
2023-03-23 11:10:06 +0000

    remove debug prints, fix metrics saving

--- generativeimage2text/vc_inference.py
+++ generativeimage2text/vc_inference.py
@@ -345,10 +345,12 @@ def multi_video_inference_dir(videos_csv, annotations_json_path, model_dir, mode
         metrics_obj.evaluate()
         
         # save metrics
-        metrics = pd.DataFrame(data={'Metric Name':[], 'Metric Value':[]})
+        metrics = pd.DataFrame(columns=['Metric Name','Metric Value'])
+        r = 1
         for metric, score in metrics_obj.eval.items():
-            metrics.append({'Metric Name':metric, 'Metric Value':score}, ignore_index=True)
-        
+            metrics.loc[r] = [metric] + [score]
+            r += 1
+
         metrics.to_csv(metrics_file, index=False)
 
 if __name__ == '__main__':
